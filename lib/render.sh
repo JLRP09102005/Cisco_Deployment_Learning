8f5da1022441ac520f957a7bffbceb087655151c
Joselu <jlrp09102005@gmail.com>
2026-06-06 17:14:18 +0200

    redner script finished, pending to create ssh script to send this config files to the devices

--- lib/render.sh
+++ lib/render.sh
@@ -5,27 +5,31 @@ render_template()
     [[ -d "$1" ]] && return
     [[ -z "$2" ]] && return
 
-    tpl_route="$1"
+    tmpfile="$(mktemp)" && truncate -s 0 "$tmpfile"
     declare -n subs_array="$2"
 
     [[ "${#subs_array[@]}" -eq 0 ]] && return
 
+    cat "$1" > "$tmpfile"
+
     local marks
-    marks="$(grep -o "__.*__" "$tpl_route")"
+    marks="$(grep -o "__.*__" "$tmpfile")"
 
     while read -r line; do
 
         if [[ "$line" == "__ENABLESECRET__" ]]; then
 
             read -s -r -p "Write the password for the privileged mode: " password
+            sed "s|$line|$password|g" "$tmpfile"
 
         elif [[ "$line" == "__USERPASS__" ]]: then
     
             read -s -r -p "Write the password for the new user: " password
+            sed "s|$line|$password|g" "$tmpfile"
 
         else
 
-            sed "s|${line}|${subs_array[$line]}|g" "$tpl_route"
+            sed "s|${line}|${subs_array[$line]}|g" "$tmpfile"
 
         fi
 
