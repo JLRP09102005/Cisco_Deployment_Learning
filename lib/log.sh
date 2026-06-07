97181bde97bf197ecb383fb9976d42ceb3f062e2
Joselu <jlrp09102005@gmail.com>
2026-06-07 17:53:10 +0200

    fixing errors from log script

--- lib/log.sh
+++ lib/log.sh
@@ -17,7 +17,7 @@ log_init()
 log_info()
 {
     [[ -z "$1" ]] && return
-    LOG_FILE="${LOG_FILE:-'/dev/null'}"
+    LOG_FILE="${LOG_FILE:-/dev/null}"
 
     local date="$(date '+%d-%m-%y_%H%M%S')"
     printf "%b%s:%s%b\n" "$BLUE" "$date" "$1" "$NC" | tee -a "$LOG_FILE"
@@ -26,7 +26,7 @@ log_info()
 log_ok()
 {
     [[ -z "$1" ]] && return
-    LOG_FILE="${LOG_FILE:-'/dev/null'}"
+    LOG_FILE="${LOG_FILE:-/dev/null}"
 
     local date="$(date '+%d-%m-%y_%H%M%S')"
     printf "%b%s:%s%b\n" "$GREEN" "$date" "$1" "$NC" | tee -a "$LOG_FILE"
@@ -35,7 +35,7 @@ log_ok()
 log_warn()
 {
     [[ -z "$1" ]] && return
-    LOG_FILE="${LOG_FILE:-'/dev/null'}"
+    LOG_FILE="${LOG_FILE:-/dev/null}"
 
     local date="$(date '+%d-%m-%y_%H%M%S')"
     printf "%b%s:%s%b\n" "$YELLOW" "$date" "$1" "$NC" | tee -a "$LOG_FILE"
@@ -44,7 +44,7 @@ log_warn()
 log_error()
 {
     [[ -z "$1" ]] && return
-    LOG_FILE="${LOG_FILE:-'/dev/null'}"
+    LOG_FILE="${LOG_FILE:-/dev/null}"
 
     local date="$(date '+%d-%m-%y_%H%M%S')"
     printf "%b%s:%s%b\n" "$RED" "$date" "$1" "$NC" | tee -a "$LOG_FILE"
